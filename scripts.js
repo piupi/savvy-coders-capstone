fa486900904d5237fda614f81522afb313e43cda
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2019-09-29 15:36:38 -0500

    Begin making HTML layout and CSS styling. Display date

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Trying to get the date to show up at top of main page
+
+const todaysDate = new Date();
+const month = todaysDate.getMonth();
+const date = todaysDate.getDate();
+const day = todaysDate.getDay();
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+const dateDisplay = `${weekdays[day]} ${months[month]} ${date}`
+
+document.getElementById('todaysDate').textContent = dateDisplay;
+
+
+// document.getElementById('todaysDate').textContent = todaysDate.toDateString(); -- dateDisplay is better than this
+
+// Idk if i want Nunito or Space Mono for header font
