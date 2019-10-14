06fc795b234e6554f6fcfce9b94883828b5ee99e
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2019-10-14 00:32:55 -0500

    Try capturing photos with getUserMedia

--- camera-pg/camera.js
+++ camera-pg/camera.js
@@ -1,26 +1,57 @@
 
-const fileInput = document.getElementById("imgInput");
-fileInput.click();
+// const fileInput = document.getElementById("imgInput");
+// fileInput.click();
 
-//Hiding the ugly file input, and making the + button click the ugly file input button
-const uglyInput = document.getElementById("imgInput");
-const plusButton = document.querySelector("button");
+// //Hiding the ugly file input, and making the + button click the ugly file input button
+// const uglyInput = document.getElementById("imgInput");
+// const plusButton = document.querySelector("button");
 
 
-//https://stackoverflow.com/questions/43320231/html5-camera-access-controlling-the-resolution
+// //https://stackoverflow.com/questions/43320231/html5-camera-access-controlling-the-resolution
 
-//Need way to make the button lead to input
-//When i click the plus button it should simulate a click on the uglyInput. This works, but
-//only if you click the plusButton twice! So I need to try something that'll work on first click
-// function clickSimulator(){
-//   plusButton.addEventListener('click', function(){
+// //Need way to make the button lead to input
+// //When i click the plus button it should simulate a click on the uglyInput. This works, but
+// //only if you click the plusButton twice! So I need to try something that'll work on first click
+// // function clickSimulator(){
+// //   plusButton.addEventListener('click', function(){
+// //   uglyInput.click();
+// //   })
+// // }
+
+// //This way is more straightforward
+// plusButton.onclick = function(){
 //   uglyInput.click();
-//   })
 // }
 
-//This way is more straightforward
-plusButton.onclick = function(){
-  uglyInput.click();
-}
-
 //TODO: Html Media Capture doesn't give you a way to specify size apparently. Need to use getUserMedia / Media Stream / Stream API / WebRTC API. But first, read all the tabs abt Html Media Capture. MDN tells you what to do with the photos once the user has captured them. Read about File API on MDN. If you don't go with getUserMedia, see how to skip the retake/use photo screen. Might have to use PhoneGap Camera framework? Read more about canvas element and how it's related to this.
+
+//Use document.createElement("image or something") when creating the pictures from firebase. take info from database and display it
+//Sign in with google?
+
+
+//Below is trying with getUserMedia. chrome now only lets you use
+//navigator.getUserMedia in secure context (https), otherwise its undefined
+
+
+(function(){
+  const video = document.getElementById('video');
+  const vendorUrl = window.URL || window.webkitURL;
+  //The "or window webkitURL" if window URL isnt available
+  navigator.getMedia = navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia;
+  //before, below was navigator.getMedia
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+  }, function(stream) {
+    // video.src = vendorUrl.createObjectURL(stream);
+    //This method no longer works,assign stream to HTMLelement.srcObject
+    video.srcObject = stream;
+    video.play();
+  }, function(error) {
+    // Error occurred
+    // error.code
+  });
+})();
