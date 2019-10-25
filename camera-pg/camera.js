092d9c5f6d0d6d2b141b89482623ebeca0e6effa
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2019-10-24 19:23:40 -0500

    Add SPA architecture

--- camera-pg/camera.js
+++ camera-pg/camera.js
@@ -12,39 +12,44 @@ const customTxt = document.getElementById("custom-text");
 //Need way to make the button lead to input
 //When i click the plus button it should simulate a click on the uglyInput. This works, but
 //only if you click the plusButton twice! So I need to try something that'll work on first click
-function clickSimulator(){
-  plusButton.addEventListener('click', function(){
-  uglyRealInput.click();
-  })
-}
-
+// function clickSimulator(){
+//   plusButton.addEventListener('click', function(){
+//   uglyRealInput.click();
+//   document.location.href ='./camera-pg/index.html' //idk
+//   })
+// }
+//I dont know if i even need that anymore
 
 //This way is more straightforward than the clickSimulator function.
 //But only works if the input is on the actual main page wtf
-// plusButton.onclick = function(){
-//   uglyRealInput.click();
-// }
+plusButton.onclick = function(){
+  uglyRealInput.click();
+}
+
+
+//i feel like event.preventDefault() would be useful somewhere here...
 
 //Show the file name. Remove this later
-uglyRealInput.addEventListener("change", function(){
-  if(uglyRealInput.value) {
-    customTxt.innerHTML = uglyRealInput.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-  } else {
-    customTxt.innerHTML = "No file chosen yet."
-  }
-})
+// uglyRealInput.addEventListener("change", function(){
+//   if(uglyRealInput.value) {
+//     customTxt.innerHTML = uglyRealInput.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+//   } else {
+//     customTxt.innerHTML = "No file chosen yet."
+//   }
+// })
 //onload??
 
-// window.onload = function(){
-//   uglyRealInput.click();
-// }
-
-//Doesnt work on mobile so I'm trying something
-
-window.addEventListener('load', function (){
+window.onload = function(){
   uglyRealInput.click();
-  console.log("AAAAAAAAAAAAAH")
-})
+}
+
+// //Doesnt work on mobile so I'm trying something
+
+// window.addEventListener('load', function (){
+//   uglyRealInput.click();
+//   console.log("AAAAAAAAAAAAAH")
+// })
+//It made no difference on mobile
 
 //Image Preview
 const imgInput = document.getElementById("imgInput");
@@ -65,8 +70,15 @@ imgInput.addEventListener("change", function(){
     });
     reader.readAsDataURL(file);
     // or toDataURL???
+    // or output.src = URL.createObjectURL(event.target.files[0])
+    // or <input onchange="document.getElementById('blah').src = window.URL.createObjectURL(this.files[0])">
+    // data URL is a huge string containing all of the file data, whereas an object URL, is just a short string referencing the file data in-memory
+    //Read carefully: readAsDataURL and createObjectURL will cache an image in the browser, then return a base64 encoded string which references the image location in cache. If browser cache is cleared, the base64 encoded string will no longer reference the image. The base64 string isn't actual image data, it's a url reference to the image, and you shouldn't save it to a database hoping to retrieve image data. I've recently made this mistake (about 3 minutes ago).
+    document.location.href ='./camera-pg/index.html'
   }
 })
+
+//TODO: could you attach that click event to the button, and after image is uploaded, then load that page instead?
 
 //TODO: Preview image before file upload? Move the uglyRealInput to the camera page.
 //TODO: Then make plusButton go to the CameraPage and press the uglyRealInput
