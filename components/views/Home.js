be8f23456faab2549ecfb175e604a40df9386e2a
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2019-10-25 14:02:58 -0500

    Add render function

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,16 +1,30 @@
+//Using figure as 'the box'
+// function generatePics(pics) {
+//   // use map and join to iterate over the pics
+//   return pics.map(({src, calories}) => `
+//     <!-- TODO: Consider simplifying markup and just using <figure> as 'the box.' -->
+//     <div class="box">
+//       <div class="time">10:00 am</div>
+//       <figure>
+//         <img src="${src}" alt="" class="food" id="box-5"></<img>
+//         <figcaption>${calories}</figcaption>
+//       </figure>
+//   </div>
+//   `).join(" ")
+// }
+
 function generatePics(pics) {
   // use map and join to iterate over the pics
   return pics.map(({src, calories}) => `
     <!-- TODO: Consider simplifying markup and just using <figure> as 'the box.' -->
     <div class="box">
       <div class="time">10:00 am</div>
-      <figure>
-        <img src="${src}" alt="" class="food" id="box-5"></<img>
-        <figcaption>${calories}</figcaption>
-      </figure>
-  </div>
+      <img src="${src}" alt="" class="food" id="box-5"></<img>
+      <div class="cals">${calories}</div>
+    </div>
   `).join(" ")
 }
+
 
 export default (st) => `
   <div class="shoebox">
--- index.js
+++ index.js
@@ -4,3 +4,13 @@ import { Main } from "./components";
 
 // console.log(state)
 // console.log(Main(state.Home));
+
+
+function render(st = state.Home) {
+  document.querySelector("#root").innerHTML = `
+  ${Main(st)}
+`;
+}
+
+
+render();
