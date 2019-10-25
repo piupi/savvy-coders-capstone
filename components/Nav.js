434cbc9a4fe68103b6b20573a965c9ab83321e29
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2019-10-25 11:25:54 -0500

    Move navigation markup into a functional component.

---
+++ components/Nav.js
@@ -0,0 +1,6 @@
+export default () => `
+  <div class="navigation">
+    <button id="settings" type="button"><span class="fas fa-cog fa-l" id=""></span></button>
+    <button id="calendar" type="button"><span class="far fa-calendar-alt fa-lg" id=""></span></button>
+  </div>
+`
